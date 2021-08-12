ca4138b2bef347a50ee38f99d7945bf3650a40e1
reddogwow <redd@seznam.cz>
2021-08-12 20:56:10 +0200

    Create Menu1.swift

---
+++ Menu1.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Menu1: View {
+    var body: some View {
+        Text("Hello, this is Menu1.swift")
+    }
+}
+
+struct Menu1_Previews: PreviewProvider {
+    static var previews: some View {
+        Menu1()
+    }
+}
